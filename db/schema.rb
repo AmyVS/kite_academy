5edc383e606bf7eb750bd2bcd30d0a27bfc57a3b
amyvs <amydecat@yahoo.com>
2014-08-29 09:20:44 -0700

    add level table and files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829051916) do
+ActiveRecord::Schema.define(version: 20140829161416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 20140829051916) do
     t.integer  "level_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "levels", force: true do |t|
+    t.string "name"
   end
 
 end
