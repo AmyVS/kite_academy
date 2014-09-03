c26528c3738c59904edf1955e4a0d510ec2addcc
amyvs <amydecat@yahoo.com>
2014-09-03 13:52:13 -0700

    pluralize controller names in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
-  match('/', {via: :get, to: 'level#index'})
+  match('/', {via: :get, to: 'levels#index'})
 
-  match('lessons', {via: :get, to: 'lesson#index'})
-  match('lessons/new', {via: :get, to: 'lesson#new'})
-  match('lessons', {via: :post, to: 'lesson#create'})
-  match('lessons/:id', {via: :get, to: 'lesson#show'})
-  match('lessons/:id/edit', {via: :get, to: 'lesson#edit'})
-  match('lessons/:id', {via: [:patch, :put], to: 'lesson#update'})
-  match('lessons/:id/destroy', {via: :delete, to: 'lesson#destroy'})
+  match('lessons', {via: :get, to: 'lessons#index'})
+  match('lessons/new', {via: :get, to: 'lessons#new'})
+  match('lessons', {via: :post, to: 'lessons#create'})
+  match('lessons/:id', {via: :get, to: 'lessons#show'})
+  match('lessons/:id/edit', {via: :get, to: 'lessons#edit'})
+  match('lessons/:id', {via: [:patch, :put], to: 'lessons#update'})
+  match('lessons/:id/destroy', {via: :delete, to: 'lessons#destroy'})
 
-  match('levels', {via: :get, to: 'level#index'})
-  match('levels/new', {via: :get, to: 'level#new'})
-  match('levels', {via: :post, to: 'level#create'})
-  match('levels/:id', {via: :get, to: 'level#show'})
-  match('levels/:id/edit', {via: :get, to: 'level#edit'})
-  match('levels/:id', {via: [:patch, :put], to: 'level#update'})
-  match('levels/:id/destroy', {via: :delete, to: 'level#destroy'})
+  match('levels', {via: :get, to: 'levels#index'})
+  match('levels/new', {via: :get, to: 'levels#new'})
+  match('levels', {via: :post, to: 'levels#create'})
+  match('levels/:id', {via: :get, to: 'levels#show'})
+  match('levels/:id/edit', {via: :get, to: 'levels#edit'})
+  match('levels/:id', {via: [:patch, :put], to: 'levels#update'})
+  match('levels/:id/destroy', {via: :delete, to: 'levels#destroy'})
 
-  match('levels/:level_id/lessons/:id', {via: :get, to: 'lesson#show'})
+  match('levels/:level_id/lessons/:id', {via: :get, to: 'lessons#show'})
 
 end
